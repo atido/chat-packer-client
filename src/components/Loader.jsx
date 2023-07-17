a6c038a41ec0c911962a60995c4e7be89fa87da5
Cedric Aim <cedric.aim@gmail.com>
2023-07-17 21:48:11 +0200

    adjust on the textarea to handle enter key

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,9 +3,9 @@ import "./Loader.css";
 export default function Loader() {
   return (
     <div class="loader">
-      <div class="dot dot-1"></div>
-      <div class="dot dot-2"></div>
-      <div class="dot dot-3"></div>
+      <div className="dot dot-1"></div>
+      <div className="dot dot-2"></div>
+      <div className="dot dot-3"></div>
     </div>
   );
 }
