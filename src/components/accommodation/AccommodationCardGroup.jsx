dc7b55f033fa49e868ddd3dd4a77725ab9349748
rach <rachelpytse@gmail.com>
2023-08-02 00:49:21 +0200

    added Swiper to FlightCardGroup and AccommodationCardGroup

--- src/components/accommodation/AccommodationCardGroup.jsx
+++ src/components/accommodation/AccommodationCardGroup.jsx
@@ -1,12 +1,38 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import "../CardGroup.css";
 import AccommodationCard from "./AccommodationCard";
 
 export default function AccommodationCardGroup({ body }) {
   return (
-    <div className="card-group acccomodation">
+    <Swiper
+    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    // spaceBetween={10}
+    slidesPerView={3}
+    navigation
+    pagination={{ clickable: true }}
+    // scrollbar={{ draggable: true }}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+    breakpoints={{
+      100: {
+        slidesPerView: 2,
+        // spaceBetween: 3,
+      },
+      640: {
+        slidesPerView: 3,
+        // spaceBetween: 5,
+      },
+      
+    }}>
       {body.map((accommodation) => (
         <AccommodationCard key={accommodation._id} accommodation={accommodation} />
       ))}
-    </div>
+    </Swiper>
   );
 }
--- src/components/flight/FlightCardGroup.jsx
+++ src/components/flight/FlightCardGroup.jsx
@@ -1,12 +1,39 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import "../CardGroup.css";
 import FlightCard from "./FlightCard";
 
 export default function FlightCardGroup({ body }) {
   return (
-    <div className="card-group flight">
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      // spaceBetween={10}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      // scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      breakpoints={{
+        100: {
+          slidesPerView: 2,
+          // spaceBetween: 3,
+        },
+        640: {
+          slidesPerView: 3,
+          // spaceBetween: 5,
+        },
+
+      }}
+    >
       {body.map((flight) => (
-        <FlightCard key={flight._id} flight={flight} />
+        <SwiperSlide><FlightCard key={flight._id} flight={flight} /></SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 }
