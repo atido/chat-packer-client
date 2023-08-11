847ef1017105a6740c145aaf57721649265bc24f
Cedric Aim <cedric.aim@gmail.com>
2023-08-11 13:44:22 +0200

    add custom request header

--- src/utils/myaxios.jsx
+++ src/utils/myaxios.jsx
@@ -1,35 +1,36 @@
-import axios from "axios";
+import axios from 'axios';
 
 const myaxios = axios.create({
   withCredentials: true,
   baseURL: `${import.meta.env.VITE_BACKEND_HOST}`,
 });
 
-myaxios.interceptors.request.use((config) => {
-  const storedToken = localStorage.getItem("authToken");
+myaxios.interceptors.request.use(config => {
+  const storedToken = localStorage.getItem('authToken');
   if (storedToken) {
     config.headers = { Authorization: `Bearer ${storedToken}` };
   }
+  config.headers = { ...config.headers, 'X-conversation-token': `Testing` };
   return config;
 });
 
 myaxios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    let message = "";
+  response => response,
+  error => {
+    let message = '';
 
     if (error.response) {
-      message = error.response.data.message || "Functional error.";
+      message = error.response.data.message || 'Functional error.';
       console.log(message, {
         data: error.response.data,
         status: error.response.status,
         headers: error.response.headers,
       });
     } else if (error.request) {
-      message = "Network error.";
+      message = 'Network error.';
       console.log(message, error.request);
     } else {
-      message = "An error occurred. Please try again later.";
+      message = 'An error occurred. Please try again later.';
       console.log(message, error.message);
     }
     return Promise.reject({ ...error, message });
