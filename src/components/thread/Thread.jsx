839b4e41d67a08c47d35dcef530c9844d2fe02c3
Cedric Aim <cedric.aim@gmail.com>
2023-08-13 13:59:07 +0200

    add react markdown handling and fix the size of input by default

--- src/components/thread/Thread.jsx
+++ src/components/thread/Thread.jsx
@@ -1,16 +1,13 @@
-import "./Thread.css";
+import ReactMarkdown from 'react-markdown';
+import './Thread.css';
 
 export default function Thread({ body, isError = false }) {
   return (
-    <div
-      className={`thread ${body.role == "assistant" ? "thread--assistant" : "thread--user"} ${
-        isError ? "thread--error" : ""
-      }`}
-    >
-      {body.role == "assistant" && (
-        <img className="thread__avatar" src="avatar-assistant.png" alt="assistant avatar" />
-      )}
-      <div className="thread__message">{body.content}</div>
+    <div className={`thread ${body.role == 'assistant' ? 'thread--assistant' : 'thread--user'} ${isError ? 'thread--error' : ''}`}>
+      {body.role == 'assistant' && <img className="thread__avatar" src="avatar-assistant.png" alt="assistant avatar" />}
+      <div className="thread__message">
+        <ReactMarkdown>{body.content}</ReactMarkdown>
+      </div>
     </div>
   );
 }
