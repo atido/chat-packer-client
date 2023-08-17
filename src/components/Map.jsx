47ba355ac49c03ffd15bff098eb901e0150e0e68
Cedric Aim <cedric.aim@gmail.com>
2023-08-17 16:18:31 +0200

    bug fixed for google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,81 +1,70 @@
-import "./Map.css"
+import { Icon } from '@iconify/react';
+import { Autocomplete, DirectionsRenderer, GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import myaxios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import './Map.css';
 
-import {
-  useJsApiLoader,
-  GoogleMap,
-  Marker,
-  Autocomplete,
-  DirectionsRenderer,
-} from '@react-google-maps/api'
-
-import { useEffect, useRef, useState } from 'react'
-
-import { Icon } from '@iconify/react'
-
-import myaxios from "axios"
-
-function Map({address}) {
+function Map({ address }) {
   const [coordinates, setCoordinates] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries: ['places'],
-  })
-  
+  });
+
   useEffect(() => {
-    const geocodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY}`;
-    myaxios(geocodingAPI)
-    .then(data => {
-      if (data.status === 'OK') {
-        setCoordinates(data.results[0].geometry.location);
-      } else {
-        console.error("Geocoding error", data);
-      }
-    })
-    .catch(error => console.error("Geocoding API error", error));
-  }, [address]);
-  
-  const center = coordinates ? { lat: coordinates.lat, lng: coordinates.lng }: { lat: 48.8584, lng: 2.2945 };
+    const geocodingAPI = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${
+      import.meta.env.VITE_REACT_APP_GOOGLE_MAPS_API_KEY
+    }`;
+    myaxios
+      .get(geocodingAPI)
+      .then(response => {
+        if (response.data.status === 'OK') {
+          setCoordinates({ lat: response.data.results[0].geometry.location.lat, lng: response.data.results[0].geometry.location.lng });
+        } else {
+          console.error('Geocoding error', response);
+        }
+      })
+      .catch(error => console.error('Geocoding API error', error));
+  }, []);
 
-  
-
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState('');
+  const [duration, setDuration] = useState('');
 
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef()
+  const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
+  const destiantionRef = useRef();
 
   if (!isLoaded) {
-    return <div className="skeleton-loader"></div>
+    return <div className="skeleton-loader"></div>;
   }
 
   async function calculateRoute() {
     if (originRef.current.value === '' || destiantionRef.current.value === '') {
-      return
+      return;
     }
-    
-    const directionsService = new google.maps.DirectionsService()
+
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destiantionRef.current.value,
-      
+
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
   }
 
   function clearRoute() {
-    setDirectionsResponse(null)
-    setDistance('')
-    setDuration('')
-    originRef.current.value = ''
-    destiantionRef.current.value = ''
+    setDirectionsResponse(null);
+    setDistance('');
+    setDuration('');
+    originRef.current.value = '';
+    destiantionRef.current.value = '';
   }
 
   return (
@@ -84,26 +73,21 @@ function Map({address}) {
         <div className="mapSearchBar">
           <div>
             <Autocomplete>
-              <input className="mapInput" type='text' placeholder='Origin' ref={originRef} />
+              <input className="mapInput" type="text" placeholder="Origin" ref={originRef} />
             </Autocomplete>
           </div>
           <div>
             <Autocomplete>
-              <input
-                className="mapInput"
-                type='text'
-                placeholder='Destination'
-                ref={destiantionRef}
-              />
+              <input className="mapInput" type="text" placeholder="Destination" ref={destiantionRef} />
             </Autocomplete>
           </div>
 
           <div className="mapBtnGp">
-            <button className="btn btn--primary" type='submit' onClick={calculateRoute}>
+            <button className="btn btn--primary" type="submit" onClick={calculateRoute}>
               Calculate Route
             </button>
-            <button onClick={clearRoute}
-            ><Icon className="clearRouteBtn" icon={"ic:baseline-close"}/>
+            <button onClick={clearRoute}>
+              <Icon className="clearRouteBtn" icon={'ic:baseline-close'} />
             </button>
           </div>
         </div>
@@ -112,40 +96,36 @@ function Map({address}) {
           <div>Duration: {duration} </div>
           <button
             onClick={() => {
-              map.panTo(center)
-              map.setZoom(15)
+              console.log('> center <', coordinates);
+              map.panTo(coordinates);
+              map.setZoom(15);
             }}
-          ><Icon className="recenterBtn" icon={"iconoir:maps-arrow"}/>
+          >
+            <Icon className="recenterBtn" icon={'iconoir:maps-arrow'} />
           </button>
         </div>
       </div>
-      <div className="mapContainer" >
-        
+      <div className="mapContainer">
         <div className="map">
-          
-            <GoogleMap
-              center={center}
-              zoom={15}
-              mapContainerStyle={{ width: '100%', height: '100%' }}
-              options={{
-                zoomControl: true,
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false,
-              }}
-              onLoad={map => setMap(map)}
-            >
-              <Marker position={center} />
-              {directionsResponse && (
-                <DirectionsRenderer directions={directionsResponse} />
-              )}
-            </GoogleMap>
-          
+          <GoogleMap
+            center={coordinates}
+            zoom={15}
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            options={{
+              zoomControl: true,
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+            onLoad={map => setMap(map)}
+          >
+            <Marker position={coordinates} />
+            {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+          </GoogleMap>
         </div>
-        
       </div>
     </>
-  )
+  );
 }
 
-export default Map
+export default Map;
